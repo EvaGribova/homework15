49b22483e078bd01a9da5f710d7d044cc842bdd5
eva.gribova <eva.gribova.93@mail.ru>
2024-03-13 15:36:38 +0400

    Code added

---
+++ src/test/java/ru/netology/tickets/AviaSoulsTest.java
@@ -0,0 +1,70 @@
+package ru.netology.tickets;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.util.Comparator;
+
+
+public class AviaSoulsTest {
+
+    Ticket tickets1 = new Ticket("Мордер", "Шир", 1_000, 13, 17);
+    Ticket tickets2 = new Ticket("Мордер", "Шир", 500, 12, 14);
+    Ticket tickets3 = new Ticket("Мордер", "Шир", 7_000, 11, 13);
+    Ticket tickets4 = new Ticket("Мордер", "Шир", 2_000, 15, 16);
+    Ticket tickets5 = new Ticket("Мордер", "Шир", 1_000, 9, 10);
+
+    @Test
+    public void shouldCompareByPrice() {
+
+        int expectedMore = 1;
+        int actualMore = tickets1.compareTo(tickets2);
+        int expectedLess = -1;
+        int actualLess = tickets4.compareTo(tickets3);
+        int expectedEquals = 0;
+        int actualEquals = tickets5.compareTo(tickets1);
+
+        Assertions.assertEquals(expectedMore, actualMore);
+        Assertions.assertEquals(expectedLess, actualLess);
+        Assertions.assertEquals(expectedEquals, actualEquals);
+    }
+
+    @Test
+    public void shouldSortByPrice() {
+        AviaSouls service = new AviaSouls();
+
+        service.add(tickets1);
+        service.add(tickets2);
+        service.add(tickets3);
+        service.add(tickets4);
+
+        Ticket[] expected = {tickets2, tickets1, tickets4, tickets3};
+        Ticket[] actual = service.search("Мордер", "Шир");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCompareTime() {
+        TicketTimeComparator comp = new TicketTimeComparator();
+
+        int expectedMore = 1;
+        int actualMore = comp.compare(tickets2, tickets5);
+        int expectedLess = -1;
+        int actualLess = comp.compare(tickets4, tickets1);
+        int expectedEquals = 0;
+        int actualEquals = comp.compare(tickets4, tickets5);
+    }
+
+    @Test
+    public void shouldFindWithComparator() {
+        TicketTimeComparator comp = new TicketTimeComparator();
+        AviaSouls service = new AviaSouls();
+
+        service.add(tickets1);
+        service.add(tickets2);
+
+        Ticket[] expected = {tickets2, tickets1};
+        Ticket[] actual = service.searchAndSortBy("Мордер", "Шир", comp);
+    }
+}
